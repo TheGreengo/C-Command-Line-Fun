747a560d283659ecddff1201d30fd8192c700f91
Braden <bradenoptimus@gmail.com>
2023-10-27 19:59:23 -0700

    got it to read from the file print tasks

--- todo.c
+++ todo.c
@@ -14,5 +14,53 @@ void removeTodo() {
 }
 
 int main(void) {
+    FILE * file;
+    char ** todos;
+    char buf;
+    size_t count = 0;
+
+    file = fopen("tasks.txt", "r");
+    while ((buf = getc(file)) != EOF) {
+        if (buf == '\n') {
+            count++;
+        }
+    }
+    fclose(file);
+
+    todos = malloc((count+1) * sizeof* todos);
+    int i = 0;
+    while (i < count) {
+        todos[i] = malloc(count * sizeof* todos[i]);
+        i++;
+    }
+
+    count = 0;
+    int select = 0;
+    file = fopen("tasks.txt", "r");
+    while ((buf = getc(file)) != EOF) {
+        if (buf == '\n') {
+            todos[count][select] = '\0';
+            count++;
+            select = 0;
+        } else {
+            todos[count][select] = buf;
+            select++;
+        }
+    }
+    fclose(file);
+
+    i = 0;
+    while (i < count) {
+        printf("%s\n", todos[i]);
+        i++;
+    }   
+
+    i = 0; 
+    while (i < count) { 
+        free(todos[i]); 
+        i++;
+    } 
+    free(todos);
+
     return 0;
 }
