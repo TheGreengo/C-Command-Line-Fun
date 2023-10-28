603970fb6d30aaaeabce7132b9219b8ea8bef2f9
Braden <bradenoptimus@gmail.com>
2023-10-28 07:30:33 -0700

    got tasks to add

--- todo.c
+++ todo.c
@@ -1,16 +1,46 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
-void printTodos() {
-
+void printTodos(char ** todos, int count) {
+    int i = 0;
+    while (i < count) {
+        printf("%s\n", todos[i]);
+        i++;
+    }
 }
 
-void addTodo() {
+// This is working except it keeps adding an extra empty line
+void addTodo(char *** todos, char * todo, size_t * count) {
+    (* count)++;
+    int i = 0;
+    char ** temp;
+    temp = malloc((*count) * sizeof* temp);
+    while (i < (* count)) {
+        temp[i] = malloc(200 * sizeof* temp[i]);
+        if (i < (* count) - 1 ) {
+            strcpy(temp[i],(* todos)[i]);
+        } else {
+            strcpy(temp[i], todo);
+        }
+        i++;
+    }
 
+    i = 0;
+    while (i < (* count) - 1) {
+        free((* todos)[i]);
+        i++;
+    }
+    free((* todos));
+
+    (*todos) = temp;
+
+    temp = NULL;
 }
 
-void removeTodo() {
+void removeTodo(char ** todos, size_t num, size_t * count) {
 
+    (* count)--;
 }
 
 int main(void) {
@@ -49,11 +79,18 @@ int main(void) {
     }
     fclose(file);
 
-    i = 0;
-    while (i < count) {
-        printf("%s\n", todos[i]);
-        i++;
-    }   
+    
+    char * buffer;
+    size_t res;
+    size_t thing = 100;
+
+    while (1) {
+        printTodos(todos, count);
+        res = getline(&buffer, &thing, stdin);
+        if (buffer[0] == 'a') { addTodo(&todos, &buffer[2], &count); }
+        if (buffer[0] == 'd') { removeTodo(todos, (((size_t) buffer[2]) - ((size_t) "0")), & count); }
+        if (buffer[0] == 'q') { break; }
+    }
 
     i = 0; 
     while (i < count) { 
