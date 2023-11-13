171a2baa673e7ccdcf92b5dcef02e4fe44956e7a
Braden <bradenoptimus@gmail.com>
2023-11-12 20:46:31 -0700

    jl;aksdf

--- calc.c
+++ calc.c
@@ -1,2 +1,7 @@
+// The hope here is to make a command line calculator where
+// I can just type in a math operation, complete with para-
+// ntheses and exponentiation, and it will print out the 
+// answer
+
 #include "stdio.h"
 
